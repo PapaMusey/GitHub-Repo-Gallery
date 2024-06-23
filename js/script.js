5252cfeef5d7b3710bbd6db64ec343df3207fe68
Andy Musey <p.emusey@gmail.com>
2024-06-23 20:13:48 +0000

    updated the UI with GitHub's APIs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 //  This div is where profile information will appears.
 const overview = document.querySelector(".overview");
+//  to select the unordered list to display the repos list.
+const repoList = document.querySelector(".repos")
+
 
 const username = "PapaMusey";
 
@@ -10,6 +13,7 @@ const getUserData = async function () {
     displayUserInformation(data); // to display user's information on the webpage
 };
 getUserData();
+
 
 // function to display the fetched user information on the page
 const displayUserInformation = function (data) {
@@ -29,3 +33,27 @@ const displayUserInformation = function (data) {
     </div>  `;
     overview.append(div);
 }
+
+// function to fetch repos.
+const getUserRepos = async function () {
+    const response = await fetch(` https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await response.json();
+    console.log(repoData);
+    displayRepoInformation(repoData);
+
+};
+getUserRepos();
+
+
+//  function to display information about each repo
+const displayRepoInformation = function(repos) {
+    for (const repo of repos){
+        let repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+
+    }
+    
+
+};
